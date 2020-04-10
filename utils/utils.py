b92beb66c7255ab50c7fbe47a8d3ce86adf5ce06
Dae-Hoon, KIM <eogns0707@gmail.com>
2020-04-10 15:51:26 +0900

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -28,41 +28,15 @@ def evaluate(model,
              net_h=416,
              net_w=416,
              save_path=None):
-    """ Evaluate a given dataset using a given model.
-    code originally from https://github.com/fizyr/keras-retinanet
 
-    # Arguments
-        model           : The model to evaluate.
-        generator       : The generator that represents the dataset to evaluate.
-        iou_threshold   : The threshold used to consider when a detection is positive or negative.
-        obj_thresh      : The threshold used to distinguish between object and non-object
-        nms_thresh      : The threshold used to determine whether two detections are duplicates
-        net_h           : The height of the input image to the model, higher value results in better accuracy
-        net_w           : The width of the input image to the model
-        save_path       : The path to save images with visualized detections to.
-    # Returns
-        A dict mapping class names to mAP scores.
-    """
-    # gather all detections and annotations
-    # print(generator.num_classes()) # 80
-    # print(generator.size()) # 502
     all_detections = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
     all_annotations = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
-
-    # print(all_detections)
-    # print(len(all_detections)) # 502
-    # print(all_annotations)
-    # print(len(all_annotations)) # 502
 
     for i in range(generator.size()):
         raw_image = [generator.load_image(i)]
 
-        # make the boxes and the labels
         pred_boxes = get_yolo_boxes(model, raw_image, net_h, net_w, generator.get_anchors(), obj_thresh, nms_thresh)[0]
 
-        # print(pred_boxes)
-        # print('########################')
-        # print(len(pred_boxes))
         score = np.array([box.get_score() for box in pred_boxes])
         pred_labels = np.array([box.label for box in pred_boxes])
 
@@ -71,23 +45,18 @@ def evaluate(model,
         else:
             pred_boxes = np.array([[]])
 
-            # sort the boxes and the labels according to scores
         score_sort = np.argsort(-score)
         pred_labels = pred_labels[score_sort]
         pred_boxes = pred_boxes[score_sort]
 
-        # copy detections to all_detections
         for label in range(generator.num_classes()):
             all_detections[i][label] = pred_boxes[pred_labels == label, :]
-            # if len(all_detections[i][label]) > 0: print(all_detections[i][label])
 
         annotations = generator.load_annotation(i)
 
-        # copy detections to all_annotations
         for label in range(generator.num_classes()):
             all_annotations[i][label] = annotations[annotations[:, 4] == label, :4].copy()
 
-    # compute mAP by comparing all detections and all annotations
     average_precisions = {}
 
     for label in range(generator.num_classes()):
@@ -122,25 +91,20 @@ def evaluate(model,
                     false_positives = np.append(false_positives, 1)
                     true_positives = np.append(true_positives, 0)
 
-        # no annotations -> AP for this class is 0 (is this correct?)
         if num_annotations == 0:
             average_precisions[label] = 0
             continue
 
-        # sort by score
         indices = np.argsort(-scores)
         false_positives = false_positives[indices]
         true_positives = true_positives[indices]
 
-        # compute false positives and true positives
         false_positives = np.cumsum(false_positives)
         true_positives = np.cumsum(true_positives)
 
-        # compute recall and precision
         recall = true_positives / num_annotations
         precision = true_positives / np.maximum(true_positives + false_positives, np.finfo(np.float64).eps)
 
-        # compute average precision
         average_precision = compute_ap(recall, precision)
         average_precisions[label] = average_precision
 
@@ -189,10 +153,8 @@ def do_nms(boxes, nms_thresh):
 def decode_netout(netout, anchors, obj_thresh, net_h, net_w):
     grid_h, grid_w = netout.shape[:2]
     nb_box = 3
-    # netout = netout.reshape((grid_h, grid_w, nb_box, -1))
     netout = tf.reshape(netout, (grid_h, grid_w, nb_box, -1))
     nb_class = netout.shape[-1] - 5
-    # print(nb_class) # 80
     boxes = []
 
     net = np.array(netout)
@@ -202,41 +164,27 @@ def decode_netout(netout, anchors, obj_thresh, net_h, net_w):
     net[..., 5:] *= net[..., 5:] > obj_thresh
     netout = net
 
-    # netout[..., :2] = _sigmoid((netout[..., :2]))
-    # netout[..., 4] = _sigmoid(netout[..., 4])
-    # netout[..., 5:] = netout[..., 4][..., np.newaxis] * _softmax(netout[..., 5:])
-    # netout[..., 5:] *= netout[..., 5:] > obj_thresh
-
     for i in range(grid_h * grid_w):
         row = i // grid_w
         col = i % grid_w
 
         for b in range(nb_box):
-            # 4th element is objectness score
             objectness = netout[row, col, b, 4]
 
-            # if (objectness <= obj_thresh): continue
+            if (objectness <= obj_thresh): continue
 
-            # first 4 elements are x, y, w, and h
             x, y, w, h = netout[row, col, b, :4]
 
-            x = (col + x) / grid_w  # center position, unit: image width
-            y = (row + y) / grid_h  # center position, unit: image height
-            w = anchors[2 * b + 0] * np.exp(w) / net_w  # unit: image width
-            h = anchors[2 * b + 1] * np.exp(h) / net_h  # unit: image height
+            x = (col + x) / grid_w
+            y = (row + y) / grid_h
+            w = anchors[2 * b + 0] * np.exp(w) / net_w
+            h = anchors[2 * b + 1] * np.exp(h) / net_h
 
-            # last elements are class probabilities
             classes = netout[row, col, b, 5:]
 
             box = BoundBox(x - w / 2, y - h / 2, x + w / 2, y + h / 2, objectness, classes)
 
             boxes.append(box)
-            # print('box.xmin : ', box.xmin)
-            # print('box.ymin : ', box.ymin)
-            # print('box.xmax : ', box.xmax)
-            # print('box.ymax : ', box.ymax)
-            # print('box.c : ', box.c)
-            # print('box.classes : ', box.classes)
 
     return boxes
 
@@ -244,7 +192,6 @@ def decode_netout(netout, anchors, obj_thresh, net_h, net_w):
 def preprocess_input(image, net_h, net_w):
     new_h, new_w, _ = image.shape
 
-    # determine the new size of the image
     if (float(net_w) / new_w) < (float(net_h) / new_h):
         new_h = (new_h * net_w) // new_w
         new_w = net_w
@@ -252,15 +199,12 @@ def preprocess_input(image, net_h, net_w):
         new_w = (new_w * net_h) // new_h
         new_h = net_h
 
-    # resize the image to the new size
     resized = cv2.resize(image[:, :, ::-1] / 255., (new_w, new_h))
 
-    # embed the image into the standard letter box
     new_image = np.ones((net_h, net_w, 3)) * 0.5
     new_image[(net_h - new_h) // 2:(net_h + new_h) // 2, (net_w - new_w) // 2:(net_w + new_w) // 2, :] = resized
     new_image = np.expand_dims(new_image, 0)
 
-    # print(new_image.shape) # (1, 416, 416, 3)
     return new_image
 
 
@@ -271,41 +215,30 @@ def normalize(image):
 def get_yolo_boxes(model, images, net_h, net_w, anchors, obj_thresh, nms_thresh):
     image_h, image_w, _ = images[0].shape
     nb_images = len(images)
-    # print(nb_images) # 1
     batch_input = np.zeros((nb_images, net_h, net_w, 3))
 
-    # preprocess the input
     for i in range(nb_images):
         batch_input[i] = preprocess_input(images[i], net_h, net_w)
-        # print(batch_input[i])
-        # print(batch_input[i].shape) # (1, 416, 416, 3)
 
-        # run the prediction
     batch_output = model.predict_on_batch(batch_input)
-    # print(batch_output) # (1, 13*a, 13*a, 255)
-    # print(len(batch_output)) # 3
     batch_boxes = [None] * nb_images
 
     for i in range(nb_images):
         yolos = [batch_output[0][i], batch_output[1][i], batch_output[2][i]]
         boxes = []
 
-        # decode the output of the network
         for j in range(len(yolos)):
             yolo_anchors = anchors[(2 - j) * 6:(3 - j) * 6]  # config['model']['anchors']
             boxes += decode_netout(yolos[j], yolo_anchors, obj_thresh, net_h, net_w)
-            # print(boxes)
-        # correct the sizes of the bounding boxes
+
         correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w)
 
-        # suppress non-maximal boxes
         do_nms(boxes, nms_thresh)
 
-        config_path = 'config.json'
-        with open(config_path) as config_buffer:
-            config = json.loads(config_buffer.read())
-        labels = config['model']['labels']
-
+        # config_path = 'config.json'
+        # with open(config_path) as config_buffer:
+        #     config = json.loads(config_buffer.read())
+        # labels = config['model']['labels']
         # draw_boxes(batch_input[i], boxes, labels, nms_thresh)
 
         batch_boxes[i] = boxes
@@ -314,16 +247,6 @@ def get_yolo_boxes(model, images, net_h, net_w, anchors, obj_thresh, nms_thresh)
 
 
 def compute_overlap(a, b):
-    """
-    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
-    Parameters
-    ----------
-    a: (N, 4) ndarray of float
-    b: (K, 4) ndarray of float
-    Returns
-    -------
-    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
-    """
     area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
 
     iw = np.minimum(np.expand_dims(a[:, 2], axis=1), b[:, 2]) - np.maximum(np.expand_dims(a[:, 0], 1), b[:, 0])
@@ -342,29 +265,14 @@ def compute_overlap(a, b):
 
 
 def compute_ap(recall, precision):
-    """ Compute the average precision, given the recall and precision curves.
-    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
-
-    # Arguments
-        recall:    The recall curve (list).
-        precision: The precision curve (list).
-    # Returns
-        The average precision as computed in py-faster-rcnn.
-    """
-    # correct AP calculation
-    # first append sentinel values at the end
     mrec = np.concatenate(([0.], recall, [1.]))
     mpre = np.concatenate(([0.], precision, [0.]))
 
-    # compute the precision envelope
     for i in range(mpre.size - 1, 0, -1):
         mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
 
-    # to calculate area under PR curve, look for points
-    # where X axis (recall) changes value
     i = np.where(mrec[1:] != mrec[:-1])[0]
 
-    # and sum (\Delta recall) * prec
     ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
     return ap
 
@@ -382,7 +290,6 @@ def draw_boxes(image, boxes, labels, obj_thresh):
     for box in boxes:
         label_str = ''
         label = -1
-        # label = 0
 
         for i in range(len(labels)):
             if box.classes[i] > obj_thresh:
@@ -391,21 +298,7 @@ def draw_boxes(image, boxes, labels, obj_thresh):
                 print(labels[i] + ': ' + str(box.classes[i] * 100) + '%')
 
         if label >= 0:
-            # cv2.rectangle(img=image, pt1=(int(box.xmin), int(box.ymin)), pt2=(int(box.xmax), int(box.ymax)), color=(0, 255, 0), thickness=3)
             _image = np.array(image)
-
-            # if box.xmin < 0:
-            #     print(box.xmin, ' < 0')
-            #     box.xmin = 0
-            # if box.ymin < 0:
-            #     print(box.ymin, ' < 0')
-            #     box.ymin = 0
-            # if box.xmax < 0:
-            #     print(box.xmax, ' < 0')
-            #     box.xmax = 0
-            # if box.ymax < 0:
-            #     print(box.ymax, ' < 0')
-            #     box.ymax = 0
 
             img = cv2.rectangle(_image, (int(box.xmin), int(box.ymin)), (int(box.xmax), int(box.ymax)), (0, 255, 0), 3)
             cv2.putText(img=img,
