4b17a4833ed84a1d6d3e5e693c6741c0f9bb0b1e
Dae-Hoon, KIM <eogns0707@gmail.com>
2020-05-06 15:41:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     training = True
     if training: yolo()
     else:
-        # arg = argparse.ArgumentParser(description='YOLO-v3 on VOC2007')
-        # arg.add_argument('-d', '--data_dir', default='./vocToolkit/', help='Input data directory')
-        # arg.add_argument('-b', '--batch_size', default=16, help='Input batch size')
-        # args = arg.parse_args()
-        # test(args)
-        test()
+        arg = argparse.ArgumentParser(description='YOLO-v3 on VOC2007')
+        arg.add_argument('-d', '--data_dir', default='./voc_dataset/voc2007/JPEGImages/', help='Input data directory')
+        arg.add_argument('-a', '--annotation_dir', default='./voc_dataset/voc2007/Annotations/', help='Input annotation directory')
+        arg.add_argument('-b', '--batch_size', default=8, help='Input batch size')
+        args = arg.parse_args()
+        test(args)
